69d4415b7ae3221c8dc49efb4727edc30b3515c3
ko$10$ko <kotenkoroman2018@gmail.com>
2024-08-18 08:48:21 +0300

    add secure false for proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const proxy = process.env.NODE_ENV === 'development'
     '/api': {
       target: 'http://localhost:5000',
       changeOrigin: true,
+      secure: false,
     },
   }
   : undefined;
